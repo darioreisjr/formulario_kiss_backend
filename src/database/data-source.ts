b2ff39534e2874ad29a7544619a299584d16bb0f
Dario Reis <guttyerresjunior1995@gmail.com>
2023-03-24 10:54:08 -0300

    adiciona banco de dados remoto

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -5,11 +5,12 @@ import { CreateUsersTable1679658833813 } from './migrations/1679658833813-Create
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "admin",
-    database: "formulariokiss",
+    url: "postgres://lkkpzjft:45IXCsaKnP9sl79NYq9pDxSPUjfHkFHs@tuffi.db.elephantsql.com/lkkpzjft",
+    // host: "localhost",
+    // port: 5432,
+    // username: "postgres",
+    // password: "admin",
+    // database: "formulariokiss",
     synchronize: true,
     logging: false,
     entities: [User],
